8e9ba081f3448a142e3c19e1bd32d27a1f198293
yungnuhh <no-reply>
2019-04-13 12:28:05 -0500

    adding html,css,js files

---
+++ time.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCRvho7D4Jlo2OLu6h-zlTC7-36BBWBZ-o",
+  authDomain: "time-b9af6.firebaseapp.com",
+  databaseURL: "https://time-b9af6.firebaseio.com",
+  projectId: "time-b9af6",
+  storageBucket: "",
+  messagingSenderId: "772839263924"
+};
+firebase.initializeApp(config);
